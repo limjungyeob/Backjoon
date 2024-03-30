ffbbf08a2f3e3024c864d4e9b3fa75ee833cecbe
limjungyeob <ehdtks1526@gmail.com>
2024-03-30 23:24:40 +0900

    master

--- P2751.java
+++ P2751.java
@@ -1,10 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class P2751 {
-    private static int tmpArr[]; //합치는 과정에서 정렬하여 원소를 담을 임시 배열
+    private static int A[],tmpArr[]; //합치는 과정에서 정렬하여 원소를 담을 임시 배열
 
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        A = new int[N+1];
+        tmpArr = new int[N+1];
+        for (int i=1; i <= N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        merge_sort(A);
+        for(int i=1; i <= N; i++) {
+            bw.write(A[i] +"\n");
+        }
+        bw.flush();
+        bw.close();
         
+    }
+    public static void merge_sort(int a[]) {
+        tmpArr = new int[a.length];
+        merge_sort(a, 0, a.length-1);
+        tmpArr = null;
     }
 
     //Top-Down 방식 구현
