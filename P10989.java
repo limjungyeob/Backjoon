e2627c4f34849786559a41f0fc784d92045b8b1b
limjungyeob <ehdtks1526@gmail.com>
2024-05-12 19:22:13 +0900

    master

--- P10989.java
+++ P10989.java
@@ -28,23 +28,29 @@ public class P10989 {
         int digit = 1;
         int count = 0;
 
+        //최대 자릿수만큼 반복하기
         while(count != max_size) {
             int bucket[] = new int[10];
             for(int i=0; i<A.length; i++) {
+                //일의 자리부터 시작하기
                 bucket[(A[i] / digit) % 10] ++;
             }
 
+            //합 배열을 이용해 index 계산하기.
             for(int i=1; i<10; i++) {
                 bucket[i] += bucket[i-1];
             }
-
+            
+            //현재 자릿수를 기준으로 정렬하기.
             for(int i=A.length-1; i>=0; i--) {
+                //다음 자릿수를 이동하기 위해 현재 자릿수 기준 정렬 데이터 저장하기.
                 output[bucket[(A[i] / digit % 10)] -1] = A[i];
                 bucket[(A[i] / digit) % 10] --;
             }
             for(int i=0; i<A.length; i++) {
                 A[i] = output[i];
             }
+            // 자릿수 증가시키기.
             digit *= 10;
             count++;
         }
